3c47737a74f30dc31e026bd7bec4974e29223451
Pratik Nath Tiwari <pratik@techalchemy.co>
2022-11-19 10:32:30 +0530

    [BUGFIX]

--- lib/src/manager/video_manager.dart
+++ lib/src/manager/video_manager.dart
@@ -141,7 +141,7 @@ class FlickVideoManager extends ChangeNotifier {
           .seekTo(Duration(hours: 0, minutes: 0, seconds: 0, milliseconds: 0));
     }
 
-    if (autoPlay && ModalRoute.of(_flickManager._context!)!.isCurrent) {
+    if (autoPlay) {
       //Chrome's autoplay policies are simple:
       //Muted autoplay is always allowed.
       if (kIsWeb) _flickManager.flickControlManager!.mute();
